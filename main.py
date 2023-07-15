47c66596ceb2b66a401a2443ed856ed2f638e261
Giulio Yongco <yongcogiulio125@gmail.com>
2023-07-15 22:36:00 +0800

    just some formatting

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', 5)
 
 
-
+# CREATE -------------------------------------------------------------
 def create():
 	print("CREATE ---------------------------------------")
 	new_id = [input("New ID?:\n")]
@@ -18,8 +18,6 @@ def create():
 	new_time = [input("New time?:\n")]
 	new_message = [input("New message?:\n")]
 	new_channel = [input("New channel?:\n")]
-
-
 
 	alarm_db = pd.read_csv('alarm_db.csv')
 	new = pd.DataFrame({
@@ -32,34 +30,28 @@ def create():
 	index = [len(alarm_db)]
 	)
 
-
-
 	alarm_db = pd.concat([alarm_db, new])
 	print(alarm_db)
 	alarm_db.to_csv('alarm_db.csv', index=False)
-	print("----------------------------------------------")
 
+# READ -------------------------------------------------------------
 def read():
 	print("READ -----------------------------------------")
 	alarm_db = pd.read_csv('alarm_db.csv')
 	print(alarm_db)
-	print("----------------------------------------------")
 
+# UPDATE -------------------------------------------------------------
 def update():
 	print("UPDATE ---------------------------------------")
 	index = int(input("Index Number?:\n"))
 	column = input("Column Name?:\n")
 	new_value = input("New value?:\n")
 
-	
-	
 	alarm_db = pd.read_csv('alarm_db.csv')
 	alarm_db.at[index, column] = new_value
-
-
-
 	alarm_db.to_csv('alarm_db.csv', index=False)
 
+# DELETE -------------------------------------------------------------
 def delete():
 	print("DELETE ---------------------------------------")
 	index = int(input("Index Number?:\n"))
@@ -68,11 +60,10 @@ def delete():
 	alarm_db = alarm_db.drop( index, axis=0)
 	alarm_db.to_csv('alarm_db.csv', index=False)
 
+# MENU ---------------------------------------------------------------
 def menu():
 	print(f"1.\tCreate\n2.\tRead\n3.\tUpdate\n4.\tDelete\n")
 	reply = input("What u want?:\n")
-
-
 
 	if(reply == '1'):
 		create()
@@ -84,8 +75,6 @@ def menu():
 		delete()
 	else:
 		print("weirdo man ka")
-
-
 
 	print("\n\n")
 	menu()
